1d1018b7b8a91e04170755d67eaa50090902c1cb
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-02-18 09:06:27 -0300

    pegged token storage functions

--- contracts/gladius-coin-emitter/src/lib.rs
+++ contracts/gladius-coin-emitter/src/lib.rs
@@ -3,16 +3,29 @@ use soroban_sdk::{contract, contractimpl, Address, Env, String};
 use soroban_token_sdk::metadata::TokenMetadata;
 
 
+
 mod gladius_coin;
+mod storage_types;  
 
 use gladius_coin::{write_metadata};
 use gladius_coin::{has_administrator, write_administrator};
+use storage_types::GladiusDataKey;
 
+
+pub fn read_pegged_token(e: &Env) -> Address {
+    let key = GladiusDataKey::PeggedToken;
+    e.storage().instance().get(&key).unwrap()
+}
+
+pub fn write_pegged_token(e: &Env, id: &Address) {
+    let key = GladiusDataKey::PeggedToken;
+    e.storage().instance().set(&key, id);
+}
 
 
 pub trait GladiusCoinEmitterTrait {
 
-    fn initialize_gladius(e: Env, admin: Address, decimal: u32, name: String, symbol: String);
+    fn initialize_gladius(e: Env, admin: Address, decimal: u32, name: String, symbol: String, pegged: Address);
 
 
 }
@@ -23,7 +36,7 @@ struct GladiusCoinEmitter;
 #[contractimpl]
 impl GladiusCoinEmitterTrait for GladiusCoinEmitter {
 
-    fn initialize_gladius(e: Env, admin: Address, decimal: u32, name: String, symbol: String) {
+    fn initialize_gladius(e: Env, admin: Address, decimal: u32, name: String, symbol: String, pegged: Address) {
         if has_administrator(&e) {
             panic!("already initialized")
         }
