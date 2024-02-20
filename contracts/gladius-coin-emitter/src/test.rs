db5e40dee06281e9f3f9c368f5287886e59c3bdf
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-02-19 21:00:23 -0300

    test: intialize_function

---
+++ contracts/gladius-coin-emitter/src/test.rs
@@ -0,0 +1,82 @@
+#![cfg(test)]
+extern crate std;
+use soroban_sdk::{
+    testutils::{Address as _},
+    Address, 
+    BytesN, 
+    Env,
+    String,
+};
+
+// TOKEN TO BE PEGGED (EURC)
+mod token {
+    soroban_sdk::contractimport!(file = "../token/target/wasm32-unknown-unknown/release/soroban_token_contract.wasm");
+    pub type TokenClient<'a> = Client<'a>;
+}
+use token::TokenClient;
+fn create_token_contract<'a>(e: &Env) -> TokenClient<'a> {
+    let token_address = &e.register_contract_wasm(None, token::WASM);
+    let token = TokenClient::new(e, token_address);
+    token
+}
+
+
+// GLADIUS COIN EMITTER CONTRACT
+pub mod gladius_coin_emitter{
+    soroban_sdk::contractimport!(file = "./target/wasm32-unknown-unknown/release/gladius_coin_emitter.wasm");
+    pub type GladiusCoinEmitterClient<'a> = Client<'a>;
+}
+use gladius_coin_emitter::GladiusCoinEmitterClient;
+
+fn create_gladius_coin_emitter<'a>(
+    e: & Env
+) -> GladiusCoinEmitterClient<'a> {
+    let gladius_coin_emitter_address = &e.register_contract_wasm(None, gladius_coin_emitter::WASM);
+    let gladius_coin_emitter_client = GladiusCoinEmitterClient::new(e, gladius_coin_emitter_address);
+    gladius_coin_emitter_client
+}
+
+// THE TEST
+pub struct GladiusCoinEmitterTest<'a> {
+    env: Env,
+    minter: Address,
+    user: Address,
+    pegged_token: TokenClient<'a>,
+    contract: GladiusCoinEmitterClient<'a>,
+}
+
+impl<'a> GladiusCoinEmitterTest<'a> {
+    fn setup() -> Self {
+
+        let env = Env::default();
+        env.mock_all_auths();
+        let user = Address::generate(&env);
+        let pegged_token_admin = Address::generate(&env); 
+        let minter = Address::generate(&env);
+        let pegged_token = create_token_contract(&env);
+
+        let name = String::from_str(&env, "EURC TOKEN");
+        let symbol = String::from_str(&env, "EURC");
+        let decimals = 7;
+
+        pegged_token.initialize(&pegged_token_admin, &decimals, &name, &symbol);
+        pegged_token.mint(&minter, &123_000_000_000_000_000_000);
+
+        let contract = create_gladius_coin_emitter(
+            &env,
+        );
+
+        env.budget().reset_unlimited();
+    
+
+        GladiusCoinEmitterTest {
+            env,
+            minter,
+            user,
+            pegged_token,
+            contract,
+        }
+    }
+}
+           
+mod initialize;
---
+++ contracts/gladius-coin-emitter/src/test/initialize.rs
@@ -0,0 +1,47 @@
+use crate::test::{GladiusCoinEmitterTest}; 
+use soroban_sdk::{String};
+//use crate::test::pair::SoroswapPairError;
+
+#[test]
+fn initialize_basic_info() {
+    let test = GladiusCoinEmitterTest::setup();
+
+    let ratio: u32 = 1000;
+    let decimals: u32 = 7;
+
+    test.contract.initialize_gladius(
+        &test.minter,
+        &test.pegged_token.address,
+        &ratio
+        );
+    
+    assert_eq!(test.contract.name(), String::from_str(&test.env, "Gladius Coin"));
+    assert_eq!(test.contract.symbol(), String::from_str(&test.env, "GLC"));
+    assert_eq!(test.contract.decimals(), decimals);
+    assert_eq!(test.contract.pegged(), test.pegged_token.address);
+    assert_eq!(test.contract.ratio(), ratio);
+    assert_eq!(test.contract.minter(), test.minter);
+}
+
+#[test]
+#[should_panic]
+fn initialize_twice() {
+    let test = GladiusCoinEmitterTest::setup();
+
+    let ratio: u32 = 1000;
+    let decimals: u32 = 7;
+
+    test.contract.initialize_gladius(
+        &test.minter,
+        &test.pegged_token.address,
+        &ratio
+        );
+    
+    test.contract.initialize_gladius(
+        &test.minter,
+        &test.pegged_token.address,
+        &ratio
+        );
+}
+
+
