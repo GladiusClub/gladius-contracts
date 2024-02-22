d30a833dfd69009db063e0cd2426f56f4b6acb3a
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-02-22 16:01:58 -0300

    setup and deploy in standalone

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # gladius-contracts
 See [./docs](./docs/README.md) for the Gladius Economics and Documentation
 
+## Setup
+```
+cp .env.example .env
+```
+Fill with your desired secret keys. If you don't set any, the scripts will create new accounts for you
+
 ## Contracts
 In one terminal
 ```
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-
 # Enable the 'exit immediately' shell option
 set -e
 
 # Accept command-line arguments
 NETWORK="$1"
-
 source /workspace/scripts/setup.sh $NETWORK
 
 # Define the arguments to be passed to the 'soroban contract' commands
 ARGS="--network $NETWORK --source gladius-admin"
-echo "Using ARGS: $ARGS"
 
-# Create a directory for Soroban files if it doesn't exist
-mkdir -p .soroban
-
-echo "--"
-echo "--"
+echo -e "${RED}===${NC}"
+echo -e "${GREEN}CONTRACTS COMPILATION${NC}"
 
 # Compile the gladius-coin-emitter contract
 echo "Compiling the gladius-coin-emitter contract"
@@ -26,88 +20,115 @@ make build
 echo "--"
 echo "--"
 
+# Compile the gladius-coin-emitter contract
+echo "Compiling the token contract"
+cd /workspace/contracts/token
+make build
+
+
 # Define the paths to the compiled WASM files
 GLADIUS_COIN_EMITTER_WASM="/workspace/contracts/gladius-coin-emitter/target/wasm32-unknown-unknown/release/gladius_coin_emitter.optimized.wasm"
+TOKEN_WASM="/workspace/contracts/token/target/wasm32-unknown-unknown/release/soroban_token_contract.wasm"
 
+echo -e "${RED}===${NC}"
+echo -e "${GREEN}PEGGED TOKEN CONTRACT DEPLOYMENT${NC}"
+
+# Deploy the TOKEN contract
+echo "Deploy and setup the Pegged Token contract"
+TOKEN_ID="$(
+  soroban contract deploy $ARGS \
+    --wasm $TOKEN_WASM
+)"
+echo "$TOKEN_ID" > /workspace/.soroban/pegged_token_id
+echo "Pegged token (EURC) deployed successfully with TOKEN_ID: $TOKEN_ID"
+
+NAME="EURC Token"
+SYMBOL="EURC"
+PEGGED_TOKEN_ADMIN_ADDRESS="$(soroban keys address token-admin)"
+echo Initializing token with NAME $NAME  and SYMBOL $SYMBOL
+
+soroban contract invoke \
+  --network $NETWORK --source token-admin \
+  --id $TOKEN_ID \
+  -- \
+  initialize \
+  --admin "$PEGGED_TOKEN_ADMIN_ADDRESS" \
+  --decimal 7 \
+  --name "$NAME" \
+  --symbol "$SYMBOL"
+
+
+echo -e "${RED}===${NC}"
+echo -e "${GREEN}GLADIUS COINT EMITTER CONTRACT DEPLOYMENT${NC}"
 # Deploy the GLADIUS_COIN_EMITTER contract
-echo "Deploy the Gladius Coint Emitter contract"
 GLADIUS_COIN_EMITTER_ID="$(
   soroban contract deploy $ARGS \
     --wasm $GLADIUS_COIN_EMITTER_WASM
 )"
 echo "$GLADIUS_COIN_EMITTER_ID" > /workspace/.soroban/gladius_coin_emitter_id
-echo "SoroswapFactory deployed successfully with GLADIUS_COIN_EMITTER_ID: $GLADIUS_COIN_EMITTER_ID"
+echo "GladiusCoinEmitter deployed successfully with GLADIUS_COIN_EMITTER_ID: $GLADIUS_COIN_EMITTER_ID"
+echo "   "
+RATIO="1000"
+echo "Initializing the GladiusCoinEmitter to use the $NAME pegged token, GLADIUS_ADMIN_ADDRESS=$GLADIUS_ADMIN_ADDRESS and RATIO=$RATIO"
+GLADIUS_ADMIN_ADDRESS="$(soroban keys address gladius-admin)"
 
-echo "--"
-echo "--"
-
-# Get the admin address
-ADMIN_ADDRESS="$(soroban keys address gladius-admin)"
-
-# echo "Initialize the SoroswapFactory contract"
-# soroban contract invoke \
-#   $ARGS \
-#   --id $FACTORY_ID \
-#   -- \
-#   initialize \
-#   --setter "$TOKEN_ADMIN_ADDRESS" \
-#   --pair_wasm_hash "$PAIR_WASM_HASH"
+soroban contract invoke \
+  $ARGS \
+  --id $GLADIUS_COIN_EMITTER_ID \
+  -- \
+  initialize \
+  --admin $GLADIUS_ADMIN_ADDRESS \
+  --pegged $TOKEN_ID \
+  --ratio $RATIO
 
 # echo "--"
 # echo "--"
 
-# FACTORY_ADDRESS="$(node /workspace/scripts/address_workaround.js $FACTORY_ID)"
-
-# # Create the new FACTORY object with the updated factory id and addresses
-# NEW_FACTORY_OBJECT="{ \"network\": \"$NETWORK\", \"factory_id\": \"$FACTORY_ID\", \"factory_address\": \"$FACTORY_ADDRESS\" }"
-# echo "New factory object: $NEW_FACTORY_OBJECT"
-# # NEW_FACTORY_OBJECT="{ \"network\": \"futurenet\", \"factory_id\": \"5adb2e4748f175bcc1ab4e11c0f03bc275701ef556cd9d2b10becb37ea6a33c9\", \"factory_address\": \"CBNNWLSHJDYXLPGBVNHBDQHQHPBHK4A66VLM3HJLCC7MWN7KNIZ4SLNG\"}"
-
-# FACTORY_FILE="/workspace/.soroban/factory.json"
-# # Initialize factory.json if it does not exist
-# if [[ ! -f "$FACTORY_FILE" ]]; then
-#     echo file not found
-#     echo "[]" > "$FACTORY_FILE"
-# fi
 
 
-# CURRENT_FACTORY_JSON=$(cat $FACTORY_FILE)
-# echo "CURRENT_FACTORY_JSON: $CURRENT_FACTORY_JSON"
+NEW_DEPLOYMENTS_OBJECT="{ \"network\": \"$NETWORK\", \"pegged_token_admin_public\": \"$PEGGED_TOKEN_ADMIN_ADDRESS\", \"gladius_admin_public\": \"$GLADIUS_ADMIN_ADDRESS\", \"token_id\": \"$TOKEN_ID\",  \"gladius_emitter_id\": \"$GLADIUS_COIN_EMITTER_ID\"}"
+
+DEPLOYMENTS_FILE="/workspace/.soroban/deployments.json"
+if [[ ! -f "$DEPLOYMENTS_FILE" ]]; then
+    # echo file not found
+    echo "[]" > "$DEPLOYMENTS_FILE"
+fi
+
+CURRENT_DEPLOYMENTS_JSON=$(cat $DEPLOYMENTS_FILE)
+# echo "CURRENT_DEPLOYMENTS_JSON: $CURRENT_DEPLOYMENTS_JSON"
 
 
-# # check if the network already exists in that json
-# exists=$(echo "$CURRENT_FACTORY_JSON" | jq '.[] | select(.network == "'$NETWORK'")')
+# check if the network already exists in that json
+exists=$(echo "$CURRENT_DEPLOYMENTS_JSON" | jq '.[] | select(.network == "'$NETWORK'")')
 # echo "This network already exist in the factory.json? : $exists"
 
-# NEW_FACTORY_JSON="{}"
-# if [[ -n "$exists" ]]; then
-#     # if the network exists, update the factory for that network
-#     echo network exists, replace
-#     NEW_FACTORY_JSON=$(echo "$CURRENT_FACTORY_JSON" | jq '
-#         map(if .network == "'$NETWORK'" then '"$NEW_FACTORY_OBJECT"' else . end)'
-#     )
-# else
-#     # if the network doesn't exist, append the new object to the list
-#     echo network does not exist, append
-#     NEW_FACTORY_JSON=$(echo "$CURRENT_FACTORY_JSON" | jq '. += ['"$NEW_FACTORY_OBJECT"']')
-# fi
+NEW_DEPLOYMENTS_JSON="{}"
+if [[ -n "$exists" ]]; then
+    # if the network exists, update the factory for that network
+    # echo "  network exists, replace"
+    NEW_DEPLOYMENTS_JSON=$(echo "$CURRENT_DEPLOYMENTS_JSON" | jq '
+        map(if .network == "'$NETWORK'" then '"$NEW_DEPLOYMENTS_OBJECT"' else . end)'
+    )
+else
+    # if the network doesn't exist, append the new object to the list
+    # echo "  network does not exist, append"
+    NEW_DEPLOYMENTS_JSON=$(echo "$CURRENT_DEPLOYMENTS_JSON" | jq '. += ['"$NEW_DEPLOYMENTS_OBJECT"']')
+fi
 
-# # echo "NEW_FACTORY_JSON: $NEW_FACTORY_JSON"
-# echo "$NEW_FACTORY_JSON" > "$FACTORY_FILE"
-
-# echo "end creating the factory" 
+# echo "NEW_DEPLOYMENTS_JSON: $NEW_DEPLOYMENTS_JSON"
+echo "$NEW_DEPLOYMENTS_JSON" > "$DEPLOYMENTS_FILE"
 
 
-# # # Save the network and factory information in a JSON file
-# # jq -n \
-# #   --arg network "$NETWORK" \
-# #   --arg factory_id "$FACTORY_ID" \
-# #   --arg factory_address "$FACTORY_ADDRESS" \
-# #   '[{"network": $network, "factory_id": $factory_id, "factory_address": $factory_address}]' \
-# #   > /workspace/.soroban/factory.json
+# # Save the network and factory information in a JSON file
+# jq -n \
+#   --arg network "$NETWORK" \
+#   --arg factory_id "$FACTORY_ID" \
+#   --arg factory_address "$FACTORY_ADDRESS" \
+#   '[{"network": $network, "factory_id": $factory_id, "factory_address": $factory_address}]' \
+#   > /workspace/.soroban/factory.json
 
 
 
-# # Output the file path and contents
-# echo "Factory information available in /workspace/.soroban/factory.json"
-# cat /workspace/.soroban/factory.json
+# Output the file path and contents
+echo "Dedployments information available in $DEPLOYMENTS_FILE"
+cat $DEPLOYMENTS_FILE
