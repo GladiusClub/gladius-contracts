e4c6c2f11e732249e71276b56cac0a160c176f32
coderipper <hello@joaquinsoza.dev>
2024-02-29 12:47:30 +0100

    added transfer example

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,6 +1,7 @@
+import { Address, nativeToScVal, xdr } from 'stellar-sdk';
 import { AddressBook } from '../utils/address_book.js';
 
-import { getTokenBalance } from '../utils/contract.js';
+import { getTokenBalance, invokeContract } from '../utils/contract.js';
 import { config } from '../utils/env_config.js';
 import { mintToken } from './mint_token.js';
 
@@ -18,12 +19,46 @@ export async function testGladius(addressBook: AddressBook) {
   );
 
   // Checking the balance of the gladius admin account
-  const balance = await getTokenBalance(
+  const balanceAdminBefore = await getTokenBalance(
     addressBook.getContractId(network, 'token_id'),
     loadedConfig.admin.publicKey(),
     loadedConfig.admin
   );
-  console.log('🚀 « EURC balance:', balance);
+  console.log('🚀 « EURC balanceAdminBefore:', balanceAdminBefore);
+
+  // Example of transfering a token with the token's transfer function from the admins account to the pegged token admin account
+  // ONLY EXAMPLE SHOULD BE REMOVED AFTER
+  console.log('-------------------------------------------------------');
+  console.log('Example of executing a method of an smart contract');
+  console.log('Making a transfer of EURC Token from gladius admin to pegged token admin');
+  console.log('-------------------------------------------------------');
+  const balanceTokenAdminBefore = await getTokenBalance(
+    addressBook.getContractId(network, 'token_id'),
+    loadedConfig.getUser('PEGGED_TOKEN_ADMIN_SECRET').publicKey(),
+    loadedConfig.getUser('PEGGED_TOKEN_ADMIN_SECRET')
+  );
+  console.log('🚀 « EURC balanceTokenAdminBefore:', balanceTokenAdminBefore);
+
+  const transferInitParams: xdr.ScVal[] = [
+    new Address(loadedConfig.admin.publicKey()).toScVal(),
+    new Address(loadedConfig.getUser('PEGGED_TOKEN_ADMIN_SECRET').publicKey()).toScVal(),
+    nativeToScVal(1000000000, { type: 'i128' }),
+  ];
+  await invokeContract('token_id', addressBook, 'transfer', transferInitParams, loadedConfig.admin);
+  // Balances after transfering
+  const balanceAdminAfter = await getTokenBalance(
+    addressBook.getContractId(network, 'token_id'),
+    loadedConfig.admin.publicKey(),
+    loadedConfig.admin
+  );
+  console.log('🚀 « EURC balanceAdminAfter:', balanceAdminAfter);
+  const balanceTokenAdminAfter = await getTokenBalance(
+    addressBook.getContractId(network, 'token_id'),
+    loadedConfig.getUser('PEGGED_TOKEN_ADMIN_SECRET').publicKey(),
+    loadedConfig.getUser('PEGGED_TOKEN_ADMIN_SECRET')
+  );
+  console.log('🚀 « EURC balanceTokenAdminAfter:', balanceTokenAdminAfter);
+  // END OF EXAMPLE
 }
 
 const network = process.argv[2];
