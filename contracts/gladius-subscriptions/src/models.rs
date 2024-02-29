d8fd75f1c5b03cd8baa8a878c1bffb219dc6c727
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-02-29 10:52:06 -0300

    model

---
+++ contracts/gladius-subscriptions/src/models.rs
@@ -0,0 +1,9 @@
+use soroban_sdk::{contracttype, Vec, Address};
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct DexDistribution {
+    pub index: i32,
+    pub path: Vec<Address>,
+    pub parts: i128,
+} 
