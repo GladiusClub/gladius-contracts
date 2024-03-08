4b609ffd7ef2a5fccc46b7358269d673cadc65f0
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-03-08 08:48:18 -0300

    event: tested student_set event

--- contracts/gladius-subscriptions/src/test/set_functions.rs
+++ contracts/gladius-subscriptions/src/test/set_functions.rs
@@ -1,6 +1,7 @@
 use soroban_sdk::{testutils::{Events, MockAuthInvoke, MockAuth}, vec, IntoVal, symbol_short};
 use crate::test::{GladiusSubscriptionsTest}; 
 use crate::test::gladius_subscriptions::GladiusSubscriptionsError;
+use crate::event::{SetSportClubEvent, SetParentEvent, SetStudentEvent};
 
 
 
@@ -62,6 +63,25 @@ fn set_is_sport_club() {
     ])
     .set_is_sport_club(&test.club_0, &true);
 
+    let sport_club_set_event = test.env.events().all().last().unwrap();
+
+    let expected_sport_club_set_event: SetSportClubEvent = SetSportClubEvent {
+        sport_club: test.club_0.clone(),
+        set: true
+    };
+
+    assert_eq!(
+        vec![&test.env, sport_club_set_event.clone()],
+        vec![
+            &test.env,
+            (
+                test.contract.address.clone(),
+                ("GladiusSubscriptions", symbol_short!("club_set")).into_val(&test.env),
+                (expected_sport_club_set_event).into_val(&test.env)
+            ),
+        ]
+    );
+
     assert_eq!(test.contract.is_sport_club(&test.club_0), true);
     assert_eq!(test.contract.is_sport_club(&test.club_1), false);
     
@@ -113,6 +133,23 @@ fn set_is_parent() {
     ])
     .set_is_parent(&test.parent_0, &true);
 
+    let parent_set_event = test.env.events().all().last().unwrap();
+    let expected_parent_set_event: SetParentEvent = SetParentEvent {
+        parent: test.parent_0.clone(),
+        set: true
+    };
+    assert_eq!(
+        vec![&test.env, parent_set_event.clone()],
+        vec![
+            &test.env,
+            (
+                test.contract.address.clone(),
+                ("GladiusSubscriptions", symbol_short!("paren_set")).into_val(&test.env),
+                (expected_parent_set_event).into_val(&test.env)
+            ),
+        ]
+    );
+
     assert_eq!(test.contract.is_parent(&test.parent_0), true);
     assert_eq!(test.contract.is_parent(&test.parent_1), false);
     
@@ -163,6 +200,23 @@ fn set_is_student() {
         }
     ])
     .set_is_student(&test.student_0, &true);
+
+    let student_set_event = test.env.events().all().last().unwrap();
+    let expected_student_set_event: SetStudentEvent = SetStudentEvent {
+        student: test.student_0.clone(),
+        set: true
+    };
+    assert_eq!(
+        vec![&test.env, student_set_event.clone()],
+        vec![
+            &test.env,
+            (
+                test.contract.address.clone(),
+                ("GladiusSubscriptions", symbol_short!("stud_set")).into_val(&test.env),
+                (expected_student_set_event).into_val(&test.env)
+            ),
+        ]
+    );
 
     assert_eq!(test.contract.is_student(&test.student_0), true);
     assert_eq!(test.contract.is_student(&test.student_1), false);
