6fca05ee25f3cd70c4b76c961c36ab97fa6c0470
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-03-20 19:06:49 -0300

    make it enumerable and create tests

--- contracts/gladius-nft/src/lib.rs
+++ contracts/gladius-nft/src/lib.rs
@@ -11,16 +11,15 @@ pub use crate::erc721traits::metadata::ERC721Metadata;
 pub use crate::types::*;
 pub use crate::storage::Storage;
 
-use soroban_sdk::{panic_with_error, Address, BytesN, Env, IntoVal, Map, String, Val, Vec};
+use soroban_sdk::{contract, contractimpl, panic_with_error, Address, BytesN, Env, IntoVal, Map, String, Val, Vec};
 
-#[cfg(test)]
-mod tests;
+mod test;
 
-#[cfg_attr(test, soroban_sdk::contract)]
-pub struct ERC721Contract;
+#[contract]
+pub struct GladiusNFTContract;
 
-#[cfg_attr(test, soroban_sdk::contractimpl)]
-impl ERC721 for ERC721Contract {
+#[contractimpl]
+impl ERC721 for GladiusNFTContract {
     fn balance_of(env: Env, owner: Address) -> u32 {
         DataKey::Balance(owner)
             .extend(&env, 1000)
@@ -53,7 +52,7 @@ impl ERC721 for ERC721Contract {
 
         if let Some(addr) = DataKey::TokenOwner(token_id).get::<Address>(&env) {
             if addr == from {
-                if cfg!(feature = "enumerable") && from != to {
+                if from != to {
                     // update enumerable datai
                     let from_index_key = DataKeyEnumerable::OwnerIndexToken(from.clone());
                     let from_token_key = DataKeyEnumerable::OwnerTokenIndex(from.clone());
@@ -150,8 +149,8 @@ impl ERC721 for ERC721Contract {
     }
 }
 
-#[cfg_attr(test, soroban_sdk::contractimpl)]
-impl ERC721Metadata for ERC721Contract {
+#[contractimpl]
+impl ERC721Metadata for GladiusNFTContract {
     fn name(env: Env) -> String {
         DatakeyMetadata::Name.get(&env).unwrap()
     }
@@ -165,9 +164,8 @@ impl ERC721Metadata for ERC721Contract {
     }
 }
 
-#[cfg(feature = "enumerable")]
-#[cfg_attr(test, soroban_sdk::contractimpl)]
-impl ERC721Enumerable for ERC721Contract {
+#[contractimpl]
+impl ERC721Enumerable for GladiusNFTContract {
     fn total_supply(env: Env) -> u32 {
         DataKeyEnumerable::IndexToken
             .get::<Vec<u32>>(&env)
@@ -190,9 +188,8 @@ impl ERC721Enumerable for ERC721Contract {
     }
 }
 
-#[cfg(feature = "burnable")]
-#[cfg_attr(test, soroban_sdk::contractimpl)]
-impl ERC721Burnable for ERC721Contract {
+#[contractimpl]
+impl ERC721Burnable for GladiusNFTContract {
     fn burn(env: Env, caller: Address, token_id: u32) {
         let owner: Address = DataKey::TokenOwner(token_id)
             .get(&env)
@@ -211,41 +208,37 @@ impl ERC721Burnable for ERC721Contract {
         DataKey::Approved(token_id).remove(&env);
         DataKey::TokenOwner(token_id).remove(&env);
 
-        if cfg!(feature = "enumerable") {
-            let mut owned_index: Vec<u32> = DataKeyEnumerable::IndexToken.get(&env).unwrap();
-            let mut owned_token_index: Map<u32, u32> =
-                DataKeyEnumerable::TokenIndex.get(&env).unwrap();
-            let from_index_key = DataKeyEnumerable::OwnerIndexToken(owner.clone());
-            let from_token_key = DataKeyEnumerable::OwnerTokenIndex(owner.clone());
+    
+        let mut owned_index: Vec<u32> = DataKeyEnumerable::IndexToken.get(&env).unwrap();
+        let mut owned_token_index: Map<u32, u32> =
+            DataKeyEnumerable::TokenIndex.get(&env).unwrap();
+        let from_index_key = DataKeyEnumerable::OwnerIndexToken(owner.clone());
+        let from_token_key = DataKeyEnumerable::OwnerTokenIndex(owner.clone());
 
-            let mut from_index: Vec<u32> =
-                from_index_key.get(&env).unwrap_or_else(|| Vec::new(&env));
-            let mut from_token: Map<u32, u32> =
-                from_token_key.get(&env).unwrap_or_else(|| Map::new(&env));
+        let mut from_index: Vec<u32> =
+            from_index_key.get(&env).unwrap_or_else(|| Vec::new(&env));
+        let mut from_token: Map<u32, u32> =
+            from_token_key.get(&env).unwrap_or_else(|| Map::new(&env));
 
-            from_index.remove(from_token.get(token_id).unwrap());
-            from_token.remove(token_id);
-            owned_index.remove(owned_token_index.get(token_id).unwrap());
-            owned_token_index.remove(token_id);
+        from_index.remove(from_token.get(token_id).unwrap());
+        from_token.remove(token_id);
+        owned_index.remove(owned_token_index.get(token_id).unwrap());
+        owned_token_index.remove(token_id);
 
-            from_index_key.set(&env, &from_index);
-            from_token_key.set(&env, &from_token);
-            DataKeyEnumerable::IndexToken.set(&env, &owned_index);
-            DataKeyEnumerable::TokenIndex.set(&env, &owned_token_index);
+        from_index_key.set(&env, &from_index);
+        from_token_key.set(&env, &from_token);
+        DataKeyEnumerable::IndexToken.set(&env, &owned_index);
+        DataKeyEnumerable::TokenIndex.set(&env, &owned_token_index);
 
-            DataKey::Balance(owner).set(&env, &from_index.len());
-        } else {
-            let balance_key = DataKey::Balance(owner.clone());
-            let balance = balance_key.get(&env).unwrap_or(0);
-            balance_key.set(&env, &(balance - 1));
-        }
+        DataKey::Balance(owner).set(&env, &from_index.len());
+        
         let v: Val = token_id.into();
         Event::Burn.publish(&env, v);
     }
 }
 
-#[cfg_attr(test, soroban_sdk::contractimpl)]
-impl ERC721Contract {
+#[contractimpl]
+impl GladiusNFTContract {
     pub fn initialize(
         env: Env,
         admin: Address,
@@ -258,17 +251,15 @@ impl ERC721Contract {
         Admin::User.set(&env, &admin);
 
         env.storage().instance().extend_ttl(10000, 10000);
-        if cfg!(feature = "metadata") {
-            env.storage().instance().set(&DatakeyMetadata::Name, &name);
-            env.storage()
-                .instance()
-                .set(&DatakeyMetadata::Symbol, &symbol);
-        }
+    
+        env.storage().instance().set(&DatakeyMetadata::Name, &name);
+        env.storage()
+            .instance()
+            .set(&DatakeyMetadata::Symbol, &symbol);
 
-        if cfg!(feature = "enumerable") {
-            DataKeyEnumerable::IndexToken.set(&env, &Vec::<u32>::new(&env));
-            DataKeyEnumerable::TokenIndex.set(&env, &Map::<u32, u32>::new(&env));
-        }
+
+        DataKeyEnumerable::IndexToken.set(&env, &Vec::<u32>::new(&env));
+        DataKeyEnumerable::TokenIndex.set(&env, &Map::<u32, u32>::new(&env));
     }
 
     pub fn upgrade(env: Env, hash: BytesN<32>) {
@@ -283,36 +274,31 @@ impl ERC721Contract {
         if !env.storage().instance().has(&DataKey::TokenOwner(token_id)) {
             DataKey::TokenOwner(token_id).set(&env, &to);
 
-            if cfg!(feature = "enumerable") {
-                let mut owned_index: Vec<u32> = DataKeyEnumerable::IndexToken.get(&env).unwrap();
-                let mut owned_token_index: Map<u32, u32> =
-                    DataKeyEnumerable::TokenIndex.get(&env).unwrap();
+        
+            let mut owned_index: Vec<u32> = DataKeyEnumerable::IndexToken.get(&env).unwrap();
+            let mut owned_token_index: Map<u32, u32> =
+                DataKeyEnumerable::TokenIndex.get(&env).unwrap();
 
-                let mut owner_index: Vec<u32> = DataKeyEnumerable::OwnerIndexToken(to.clone())
+            let mut owner_index: Vec<u32> = DataKeyEnumerable::OwnerIndexToken(to.clone())
+                .get(&env)
+                .unwrap_or_else(|| Vec::new(&env));
+            let mut owner_token_index: Map<u32, u32> =
+                DataKeyEnumerable::OwnerTokenIndex(to.clone())
                     .get(&env)
-                    .unwrap_or_else(|| Vec::new(&env));
-                let mut owner_token_index: Map<u32, u32> =
-                    DataKeyEnumerable::OwnerTokenIndex(to.clone())
-                        .get(&env)
-                        .unwrap_or_else(|| Map::new(&env));
+                    .unwrap_or_else(|| Map::new(&env));
 
-                owned_token_index.set(token_id, owned_index.len());
-                owned_index.push_back(token_id);
+            owned_token_index.set(token_id, owned_index.len());
+            owned_index.push_back(token_id);
 
-                owner_token_index.set(token_id, owner_index.len());
-                owner_index.push_back(token_id);
+            owner_token_index.set(token_id, owner_index.len());
+            owner_index.push_back(token_id);
 
-                DataKeyEnumerable::IndexToken.set(&env, &owned_index);
-                DataKeyEnumerable::TokenIndex.set(&env, &owned_token_index);
-                DataKeyEnumerable::OwnerIndexToken(to.clone()).set(&env, &owner_index);
-                DataKeyEnumerable::OwnerTokenIndex(to.clone()).set(&env, &owner_token_index);
+            DataKeyEnumerable::IndexToken.set(&env, &owned_index);
+            DataKeyEnumerable::TokenIndex.set(&env, &owned_token_index);
+            DataKeyEnumerable::OwnerIndexToken(to.clone()).set(&env, &owner_index);
+            DataKeyEnumerable::OwnerTokenIndex(to.clone()).set(&env, &owner_token_index);
 
-                DataKey::Balance(to.clone()).set(&env, &owner_index.len());
-            } else {
-                let key = DataKey::Balance(to.clone());
-                let balance: u32 = key.get(&env).unwrap_or(0);
-                key.set(&env, &(balance + 1));
-            }
+            DataKey::Balance(to.clone()).set(&env, &owner_index.len());
         }
         let mut v: Vec<Val> = Vec::new(&env);
         v.push_back(to.into_val(&env));
---
+++ contracts/gladius-nft/src/test.rs
@@ -0,0 +1,57 @@
+#![cfg(test)]
+extern crate std;
+
+use soroban_sdk::{
+    testutils::{Address as _},
+    Address, 
+    Env,
+};
+
+// GLADIUS TOKEN CONTRACT
+pub mod gladius_nft{
+    soroban_sdk::contractimport!(file = "./target/wasm32-unknown-unknown/release/gladius_nft.wasm");
+    pub type GladiusNFTClient<'a> = Client<'a>;
+}
+use gladius_nft::GladiusNFTClient;
+
+fn create_gladius_nft<'a>(
+    e: & Env
+) -> GladiusNFTClient<'a> {
+    let gladius_nft_address = &e.register_contract_wasm(None, gladius_nft::WASM);
+    let gladius_nft_client = GladiusNFTClient::new(e, gladius_nft_address);
+    gladius_nft_client
+}
+
+// THE TEST
+pub struct GladiusNFTTest<'a> {
+    env: Env,
+    admin: Address,
+    user: Address,
+    contract: GladiusNFTClient<'a>,
+}
+
+impl<'a> GladiusNFTTest<'a> {
+    fn setup() -> Self {
+
+        let env = Env::default();
+        env.mock_all_auths();
+        let user = Address::generate(&env);
+        let admin = Address::generate(&env); 
+
+        let contract = create_gladius_nft(
+            &env,
+        );
+
+        env.budget().reset_unlimited();
+    
+
+        GladiusNFTTest {
+            env,
+            admin,
+            user,
+            contract,
+        }
+    }
+}
+
+mod initialize;
