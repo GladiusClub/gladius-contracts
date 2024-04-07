57e6a179f50909c7d33ea605e6745b0863da6a3b
Alexey <as.lexus@gmail.com>
2024-04-07 12:34:51 +0300

    convert image_to_pinata to typescript

---
+++ scripts/image_to_pinata.ts
@@ -0,0 +1,66 @@
+import fs from 'fs';
+import path from 'path';
+import pinataSDK from '@pinata/sdk';
+
+const PINATA_API_KEY: string | undefined = process.env.PINATA_API_KEY;
+const PINATA_API_SECRET: string | undefined = process.env.PINATA_API_SECRET;
+
+console.log("🚀 ~ PINATA_API_SECRET:", PINATA_API_SECRET);
+console.log("🚀 ~ PINATA_API_KEY:", PINATA_API_KEY);
+
+const filePath: string = '/workspace/img/golden_badge.jpg';
+const filename: string = path.basename(filePath);
+const baseName: string = path.parse(filename).name;
+
+export async function pinFileToIPFS(): Promise<void> {
+    if (!PINATA_API_KEY || !PINATA_API_SECRET) {
+        console.error("Pinata API Key or Secret is undefined.");
+        return;
+    }
+  
+    const pinata = new pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+    const stream = fs.createReadStream(filePath);
+    const options = {
+        pinataMetadata: {
+            name: `${baseName}.jpg`,
+        },
+    };
+
+    // Pin the file to IPFS
+    const result = await pinata.pinFileToIPFS(stream, options);
+    console.log(result);
+    const ipfsHash = result.IpfsHash;
+
+    const jsonOptions = {
+      pinataMetadata: {
+        name: `${baseName}.json`,
+      },
+    };
+
+    const jsonContent = {
+        name: "gladius golden nft",
+        img_url: `https://gateway.pinata.cloud/ipfs/${ipfsHash}`
+    };
+
+    // Pin the JSON content to IPFS
+    const jsonResult = await pinata.pinJSONToIPFS(jsonContent, jsonOptions);
+    const nft_uri = `https://gateway.pinata.cloud/ipfs/${jsonResult.IpfsHash}`;
+    console.log("NFT URI:", nft_uri);
+
+    const nftUriFilePath = '/workspace/.soroban/nft_uri';
+
+    // Write the NFT URI to a file
+    await fs.promises.writeFile(nftUriFilePath, nft_uri);
+    console.log('NFT URI has been written to the file successfully.');
+    return nft_uri;
+}
+
+(async () => {
+    try {
+      const nftUri = await pinFileToIPFS();
+      console.log('NFT URI:', nftUri);
+    } catch (error) {
+      console.error('Error pinning file to IPFS:', error);
+    }
+  })();
+  
