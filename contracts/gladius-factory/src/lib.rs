4562e46aeaff42bfca1ec87e0fe8e323a925e029
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-05-07 13:52:32 -0300

    initialize coin emitter

--- contracts/gladius-factory/src/lib.rs
+++ contracts/gladius-factory/src/lib.rs
@@ -9,6 +9,7 @@ use soroban_sdk::token::Client as TokenClient;
 mod create_contract;
 mod storage;
 mod error;
+mod coin_emitter;
 
 use storage::*;
 use create_contract::create_contract;
@@ -23,9 +24,11 @@ pub trait GladiusFactoryTrait {
 
     fn create_premium_club(
         e: Env,
-        sport_club_address: Address,
-        sport_club_name: String
-    );
+        admin: Address, 
+        sport_club_name: String,
+        pegged: Address,
+        ratio: u32
+    ) -> (Address, Address, Address) ;
 
 }
 
@@ -137,49 +140,45 @@ fn initialize(
 
 fn create_premium_club(
     e: Env,
-    sport_club_address: Address,
-    sport_club_name: String
+    admin: Address, 
+    sport_club_name: String,
+    pegged: Address,
+    ratio: u32
 // ) -> Result<Address, FactoryError> {
-) {
+) -> (Address, Address, Address) {
     // if !has_total_pairs(&e) {
     //     return Err(FactoryError::NotInitialized);
     // }
 
     extend_instance_ttl(&e);
 
+    // Install and Deploy Contracts
     let coin_emitter_address = create_contract(
         &e,
         get_coin_emitter_wasm_hash(&e).unwrap(),
-        &sport_club_address,
+        &admin,
         &sport_club_name    
     );
 
     let nft_address = create_contract(
         &e,
         get_nft_wasm_hash(&e).unwrap(),
-        &sport_club_address,
+        &admin,
         &sport_club_name    
     );
 
     let subscriptions_address = create_contract(
         &e,
         get_subscriptions_wasm_hash(&e).unwrap(),
-        &sport_club_address,
+        &admin,
         &sport_club_name    
     );
 
-
-    // if get_pair_exists(&e, token_pair.clone()) {
-    //     return Err(FactoryError::CreatePairAlreadyExists);
-    // }
-
-    // let pair_address = create_contract(&e, pair_wasm_hash, &token_pair);
-
-    // pair::Client::new(&e, &pair_address).initialize(
-    //     &e.current_contract_address(),
-    //     &token_pair.token_0(), 
-    //     &token_pair.token_1()
-    // );
+    coin_emitter::Client::new(&e, &coin_emitter_address).initialize(
+        &admin,
+        &pegged, 
+        &ratio
+    );
 
     // put_pair_address_by_token_pair(&e, token_pair.clone(), &pair_address);
     // add_pair_to_all_pairs(&e, &pair_address);
@@ -187,6 +186,9 @@ fn create_premium_club(
     // event::new_pair(&e, token_pair.token_0().clone(), token_pair.token_1().clone(), pair_address.clone(), get_total_pairs(&e));
 
     // Ok(pair_address)
+    (   coin_emitter_address,
+        nft_address,
+        subscriptions_address)
 }
 
 
